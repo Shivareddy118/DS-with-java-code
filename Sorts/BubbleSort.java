362321aefcd4a3adebfc43184574ca78a2aac000
Shivareddy118 <2311cs010177@mallareddyuniversity.ac.in>
2024-08-11 23:02:09 +0530

    First Commit

---
+++ Sorts/BubbleSort.java
@@ -0,0 +1,39 @@
+package Sorts;
+import java.util.Arrays;
+public class BubbleSort {
+ public void bubbleSort(int[] intArray) {
+ for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0;
+ lastUnsortedIndex--) {
+ for (int i = 0; i < lastUnsortedIndex; i++) {
+ if (intArray[i] > intArray[i + 1]) {
+ swap(intArray, i, i + 1);
+ }
+ }
+ }
+ }
+ public static void swap(int[] array, int i, int j) {
+ if (i == j) {
+ return;
+ }
+ int temp = array[i];
+ array[i] = array[j];
+ array[j] = temp;
+ }
+ public static void main(String[] args) {
+ BubbleSort bs = new BubbleSort();
+ int array[] = {5, 1, 2, 4, 3};
+ System.out.println(Arrays.toString(array));
+ bs.bubbleSort(array);
+ System.out.println(Arrays.toString(array));
+ }
+}
+
+
+
+
+
+
+
+
+
+
